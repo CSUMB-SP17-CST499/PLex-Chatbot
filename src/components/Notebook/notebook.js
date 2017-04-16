2f6851aa91fb18a7f93912b8b0e35c99f3a0c443
Will Czubiak <willczubiak@gmail.com>
2017-04-15 19:08:01 -0700

    created the basic component for the notebook items

--- src/components/Notebook/notebook.js
+++ src/components/Notebook/notebook.js
@@ -8,23 +8,41 @@ import config from '../../../config/server-info'
 export class Notebook extends Component{
     constructor(props){
         super(props);
-        
-    
+        this.state={
+          'item':[{
+            'name' : '',
+            'price' : '',
+            'image' : '',
+            'description' : ''
+
+          }]
+        }
+
         this.componentDidMount = this.componentDidMount.bind(this);
     }
-    
+
     componentDidMount(){
-        
+
     }
-    
+
     render(){
-        
+        let items = this.state.items.map((n, index) =>
+          <div className="item">
+              <span className='itemName'>
+                {n.name}
+              </span>
+              <img className='itemPic' src={n.image}>
+              <div className='itemDescription'>
+                {n.description}
+              </div>
+          </div>
+      )
         return(
             <div>
-            
+
             </div>
-        
+
         );
     }
-    
+
 }
